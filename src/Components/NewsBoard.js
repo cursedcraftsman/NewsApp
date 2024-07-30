5f54d02b3f349f6bf4b1177039f2f74f4cd56ca0
Vyshnavi <cursedcraftsman@gmail.com>
2024-07-30 23:28:08 +0530

    added images and made responsive

--- src/Components/NewsBoard.js
+++ src/Components/NewsBoard.js
@@ -1,3 +1,5 @@
+
+/*
 import { useEffect, useState } from "react"
 import { NewsItem } from "./NewsItem";
 
@@ -26,3 +28,35 @@ export const NewsBoard = ({category}) => {
     </div>
   )
 }
+*/
+// src/Components/NewsBoard.jsx
+import React, { useEffect, useState } from "react";
+import { NewsItem } from "./NewsItem";
+
+export const NewsBoard = ({ category }) => {
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    const url = `https://newsapi.org/v2/top-headlines?country=in&category=${category}&apiKey=2190a78ec0f2442ab0d682de46e9b472`;
+    fetch(url)
+      .then(response => response.json())
+      .then(data => setArticles(data.articles));
+  }, [category]);
+
+  return (
+    <div className="container">
+      <h2 className="text-center my-4">Latest <span className="badge bg-danger">News</span></h2>
+      <div className="row">
+        {articles.map((news, index) => (
+          <NewsItem
+            key={index}
+            title={news.title}
+            description={news.description}
+            src={news.urlToImage}
+            url={news.url}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -1,4 +1,4 @@
-import newsImage from '../images/newsimg.jpeg'
+/*import newsImage from '../images/newsimg.jpeg'
 export const NewsItem = ({title,description,src,url}) => {
   return (
     <div className="card bg-dark text-light mb-3 d-inline-block my-3 mx-4 px-2 py-2" style={{maxWidth:"360px"}}>
@@ -10,4 +10,64 @@ export const NewsItem = ({title,description,src,url}) => {
   </div>
 </div>
   )
-}
+}*/
+
+
+/*import newsImage1 from '../images/newsimg1.webp';
+import newsImage2 from '../images/newsimg2.jpeg';
+import newsImage3 from '../images/newsimg3.jpeg';
+import newsImage4 from '../images/newsimg4.jpeg';
+
+const randomImage = () => {
+  const images = [newsImage1, newsImage2, newsImage3, newsImage4];
+  return images[Math.floor(Math.random() * images.length)];
+};
+
+export const NewsItem = ({title, description, src, url}) => {
+  const displayImage = src || randomImage();
+  
+  return (
+    <div className="card bg-dark text-light mb-3 d-inline-block my-3 mx-4 px-2 py-2" style={{maxWidth: "360px"}}>
+      <img src={displayImage} style={{height: "200px", width: "340px"}} className="card-img-top" alt="..."/>
+      <div className="card-body">
+        <h5 className="card-title">{title.slice(0, 50)}</h5>
+        <p className="card-text">
+          {description ? description.slice(0, 90) : "NewsBoard is an online news viewing website. This is a demo para shown when our API is unable to fetch data from sites."}
+        </p>
+        <a href={url} className="btn btn-primary">Read More</a>
+      </div>
+    </div>
+  );
+};*/
+
+// src/Components/NewsItem.jsx
+import React from 'react';
+import newsImage1 from '../images/newsimg1.webp';
+import newsImage2 from '../images/newsimg2.jpeg';
+import newsImage3 from '../images/newsimg3.jpeg';
+import newsImage4 from '../images/newsimg4.jpeg';
+
+const randomImage = () => {
+  const images = [newsImage1, newsImage2, newsImage3, newsImage4];
+  return images[Math.floor(Math.random() * images.length)];
+};
+
+export const NewsItem = ({ title, description, src, url }) => {
+  const displayImage = src || randomImage();
+
+  return (
+    <div className="col-12 col-sm-6 col-md-6 col-lg-4 mb-4">
+      <div className="card bg-dark text-light h-100" style={{ maxWidth: "100%" }}>
+        <img src={displayImage} className="card-img-top" alt="News" style={{ height: "200px", objectFit: "cover" }}/>
+        <div className="card-body d-flex flex-column">
+          <h5 className="card-title">{title.slice(0, 50)}</h5>
+          <p className="card-text">
+            {description ? description.slice(0, 90) : "NewsBoard is an online news viewing website. This is a demo paragraph shown when our API is unable to fetch data from sites."}
+          </p>
+          <a href={url} className="btn btn-primary mt-auto">Read More</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
