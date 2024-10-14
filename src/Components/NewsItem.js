2ba5824016445be6bee4e57f869458ac9efcd6cd
Vyshnavi <cursedcraftsman@gmail.com>
2024-10-14 11:32:39 +0530

    first commit

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -1,46 +1,8 @@
-/*import newsImage from '../images/newsimg.jpeg'
-export const NewsItem = ({title,description,src,url}) => {
-  return (
-    <div className="card bg-dark text-light mb-3 d-inline-block my-3 mx-4 px-2 py-2" style={{maxWidth:"360px"}}>
-  <img src={src?src:newsImage} style={{height:"200px" , width:"340px"}}className="card-img-top" alt="..."/>
-  <div className="card-body">
-    <h5 className="card-title">{title.slice(0,50)}</h5>
-    <p className="card-text">{description?description.slice(0,90):"NewsBoard is online news viewing website. This is a demo para shown when our api  is unable to fetch data from sites."}</p>
-    <a href={url} className="btn btn-primary">Read More</a>
-  </div>
-</div>
-  )
-}*/
+ 
 
-
-/*import newsImage1 from '../images/newsimg1.webp';
-import newsImage2 from '../images/newsimg2.jpeg';
-import newsImage3 from '../images/newsimg3.jpeg';
-import newsImage4 from '../images/newsimg4.jpeg';
-
-const randomImage = () => {
-  const images = [newsImage1, newsImage2, newsImage3, newsImage4];
-  return images[Math.floor(Math.random() * images.length)];
-};
-
-export const NewsItem = ({title, description, src, url}) => {
-  const displayImage = src || randomImage();
-  
-  return (
-    <div className="card bg-dark text-light mb-3 d-inline-block my-3 mx-4 px-2 py-2" style={{maxWidth: "360px"}}>
-      <img src={displayImage} style={{height: "200px", width: "340px"}} className="card-img-top" alt="..."/>
-      <div className="card-body">
-        <h5 className="card-title">{title.slice(0, 50)}</h5>
-        <p className="card-text">
-          {description ? description.slice(0, 90) : "NewsBoard is an online news viewing website. This is a demo para shown when our API is unable to fetch data from sites."}
-        </p>
-        <a href={url} className="btn btn-primary">Read More</a>
-      </div>
-    </div>
-  );
-};*/
 
 // src/Components/NewsItem.jsx
+
 import React from 'react';
 import newsImage1 from '../images/newsimg1.webp';
 import newsImage2 from '../images/newsimg2.jpeg';
